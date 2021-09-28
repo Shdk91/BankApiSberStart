8eba15df0080dd18d8ad693686f3400a0968b915
Козлов Дмитрий Андреевич <DAndKozlov@sberbank.ru>
2021-09-28 12:09:43 +0300

    Добавил несколько комментариев по коду

--- src/main/java/com/example/bankapisberstart/cache/RequestCache.java
+++ src/main/java/com/example/bankapisberstart/cache/RequestCache.java
@@ -2,6 +2,15 @@ package com.example.bankapisberstart.cache;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * Данный интерфейс предназначен для реализации иденпотентности.
+ * На каждый метод каждый метод АПИ, который нуждается в реализации механизма.
+ * Создается наследник с параметром ДТО и одним полем Map <LocalDAteTime, DTO>
+ * В случае успешной обработки контроллером в Map кешируется ДТО.
+ * При обработке запросов проверяется наличие данного запроса в кэше.
+ * В методе Clear реализуется механизм очистки кэша.
+ * @param <T>
+ */
 @Component
 public interface RequestCache<T> {
     void addRequest(T t);
